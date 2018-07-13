3ffa36af55c6878e7cccc15b98f7194dab005e32
qnguyen97 <38411093+qnguyen97@users.noreply.github.com>
2018-07-13 16:51:58 -0700

    Front-end

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,24 +4,17 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomePage from './HomePage';
 import NavBar from './NavBar';
+import Login from './Login';
+import Register from './Register';
+import Homepage from './Homepage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">FACCINE: The most helpful tool for managing your family's health</h1>
-          <h4> Managing your family's timeline for vaccinations </h4>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Router>
-          <div>
-            <NavBar />
-            <Route name="home" exact path="/" component={HomePage} />
-          </div>
-        </Router>
+        <Login />
+        <Register />
+        <Homepage />
       </div>
     );
   }
